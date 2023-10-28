bc041e9c74543f2bd37d9df09220e2d111ffec47
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2023-10-28 15:17:45 +0300

    works with args

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { movies, directors } from './data.js';
 
 // The GraphQL schema
 const typeDefs = `#graphql
-  type Query {
-    hello: String
-  }
+type Query {
+  director(id: ID!): Director!
+  movie(id: ID!): Movie!
+}
+
+type Director {
+  id: ID!
+  name: String!
+  birth: Int
+}
+
+type Movie {
+  id: ID!
+  title: String!
+  description: String!
+  year: Int!
+}
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    director: (parent, args) => {
+      return directors.find((director) => director.id === args.id);
+    },
+    movie: (parent, args) => {
+      return movies.find((movie) => movie.id === args.id);
+    },
   },
 };
 
@@ -22,3 +42,6 @@ const server = new ApolloServer({
 
 const { url } = await startStandaloneServer(server);
 console.log(`🚀 Server ready at ${url}`);
+
+// parent ilişkisel yapılarda kullanacağımız parametredir.
+// args bize gelen tüm gelen parametrelerdir
